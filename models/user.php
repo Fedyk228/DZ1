458f1b340d1fdf6d984c1b46c7121553c85cff5b
Феодор <Viacheslav14rus@gmail.com>
2023-10-13 14:39:59 +0300

    Выполнен функционал Регистарции

---
+++ models/user.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+class User extends ActiveRecord
+{
+    static public function tableName()
+    {
+        return 'users';
+    }
+
+    public function rules()
+    {
+        return[
+          [['email'], 'required', 'massage' => 'Email filed required'],
+          [['password'], 'required', 'massage' => 'Password filed required'],
+          ['email', 'email'],
+          ['phone', 'string'],
+          ['fio', 'string'],
+          ['date_create', 'string'],
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return[
+            'fio' => \Yii::t('app', 'Enter your name'),
+            'email' => \Yii::t('app', 'Enter your email address'),
+            'password' => \Yii::t('app', 'Enter your password'),
+            'phone' => \Yii::t('app', 'Enter your phone'),
+        ];
+    }
+
+}
