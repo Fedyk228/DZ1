4a25f29e3b7daba888535639fbc9e776cbcf9c12
Феодор <Viacheslav14rus@gmail.com>
2023-10-13 14:41:51 +0300

    Fix регистрации

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -2,13 +2,11 @@
 
 namespace app\controllers;
 
+use app\models\User;
 use Yii;
-use yii\filters\AccessControl;
+
 use yii\web\Controller;
-use yii\web\Response;
-use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\ContactForm;
+
 
 class UserController extends Controller
 {
@@ -20,7 +18,29 @@ class UserController extends Controller
 
     public function actionRegister()
     {
-        return $this->render('register');
+        $model = new User();
+        $err = '';
+        $success = '';
+
+        if ($model->load(Yii::$app->request->post())) {
+            $exist = User::find()->where(['email' => $model->email])->asArray()->one();
+
+            if (Yii::$app->request->post('user')['password'] != Yii::$app->request->post('r_password')) {
+                $err = 'Repeat password incorrect';
+            } else {
+                $model->date_create = Data('d.m.Y - H:i');
+                $model->password = md5($model->password);
+
+                if ($model->save()) {
+                    $model->password = '';
+                    $success = 'Register success';
+                } else {
+                    $err = 'error';
+                }
+            }
+        }
+
+        return $this->render('register', ['model' => $model, 'err' => $err, 'success' => $success]);
     }
 
     public function actionLogin()
